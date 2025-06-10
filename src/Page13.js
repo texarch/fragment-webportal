bfd440ace1d91a2fe4f5a30ef924e624051af2ad
Anupama Burman <anupamaburman11@gmail.com>
2025-06-10 13:03:14 +0530

    new2

--- src/Page13.js
+++ src/Page13.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Page13.css';
 import page1bg from './page1bg.png';
-import page13image from './page13image.png';
+import grey from './page13image.png';
 
 const Page13 = () => {
   return (
     <div className="page13-container">
       <div className="page13-left-section" style={{ backgroundImage: `url(${page1bg})` }}>
         <div className="page13-image-wrapper">
-          <img src={page13image} alt="Page 13" className="page13-image" />
-          <h1 className="page13-plan-text">Plan_AI </h1>
+          <img src={grey} alt="Page 18" className="page13-image" />
+          <h1 className="page13-plan-text">Plan</h1>
         </div>
       </div>
       <div className="page13-right-section"></div>
