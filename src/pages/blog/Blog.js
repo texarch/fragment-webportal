a54e967bd36f1b9f4a1486eae6899689fd134e60
Aishani Bandyopadhyay <aishanibandyopadhyay07@gmail.com>
2025-12-18 11:44:55 +0530

    Second page for blog

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,49 +1,12 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Blog.css";
-import BlogImage from "../../assets/Blog_Page_Image.png";
-import BgPattern from "../../assets/background-pattern.png";
+import React from 'react';
+import BlogHero from './BlogHero';
+import BlogPage2 from './blog_page2';
 
 const Blog = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="blog-wrapper">
-      {/* Navbar spacing */}
-      <div className="blog-navbar"></div>
-
-      {/* Hero Section */}
-      <section className="blog-hero-section">
-        {/* Background pattern */}
-        <img
-          src={BgPattern}
-          alt="background pattern"
-          className="blog-bg-pattern"
-        />
-
-        {/* Left content */}
-        <div className="blog-content">
-          <h1>Title of the most recent Blog?!</h1>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec
-            nisi elit. Pellentesque vitae sollicitudin massa Lorem ipsum dolor
-            sit amet, consectetur vitae sollicitudin massa ......
-          </p>
-
-          <button
-            className="read-more-btn"
-            onClick={() => navigate('/blog/post')}
-          >
-            Read More
-          </button>
-        </div>
-
-        {/* Right image */}
-        <div className="blog-image">
-          <img src={BlogImage} alt="Blog" />
-        </div>
-      </section>
+    <div>
+      <BlogHero />
+      <BlogPage2 />
     </div>
   );
 };
---
+++ src/pages/blog/BlogHero.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Blog.css";
+import BlogImage from "../../assets/Blog_Page_Image.png";
+import BgPattern from "../../assets/background-pattern.png";
+
+const BlogHero = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="blog-wrapper">
+            {/* Navbar spacing */}
+            <div className="blog-navbar"></div>
+
+            {/* Hero Section */}
+            <section className="blog-hero-section">
+                {/* Background pattern */}
+                <img
+                    src={BgPattern}
+                    alt="background pattern"
+                    className="blog-bg-pattern"
+                />
+
+                {/* Left content */}
+                <div className="blog-content">
+                    <h1>Title of the most recent Blog?!</h1>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec
+                        nisi elit. Pellentesque vitae sollicitudin massa Lorem ipsum dolor
+                        sit amet, consectetur vitae sollicitudin massa ......
+                    </p>
+
+                    <button
+                        className="read-more-btn"
+                        onClick={() => navigate('/blog/post')}
+                    >
+                        Read More
+                    </button>
+                </div>
+
+                {/* Right image */}
+                <div className="blog-image">
+                    <img src={BlogImage} alt="Blog" />
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default BlogHero;
