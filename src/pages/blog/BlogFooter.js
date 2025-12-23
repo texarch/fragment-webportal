c2ada5e8b765cdc3772052c405ae82ae9f4faf81
Aishani Bandyopadhyay <aishanibandyopadhyay07@gmail.com>
2025-12-23 12:40:54 +0530

    Fixed the alignment for the last page of the blog

--- src/pages/blog/BlogFooter.js
+++ src/pages/blog/BlogFooter.js
@@ -6,7 +6,7 @@ const BlogFooter = () => {
     return (
         <footer className="blog-footer">
             <div className="blog-footer-content">
-                <div className="blog-footer-column blog-footer-left">
+                <div className="blog-footer-column blog-footer-connect">
                     <p className="blog-connect-text">Connect with us on</p>
                     <img
                         src={connections_page11}
@@ -27,18 +27,21 @@ const BlogFooter = () => {
                         {/* YouTube (120-160px) */}
                         <area shape="rect" coords="120,0,160,40" href="https://www.youtube.com/@thefragmentapp" target="_blank" rel="noopener noreferrer" alt="YouTube" />
                     </map>
+                </div>
+
+                <div className="blog-footer-column blog-footer-left">
                     <p className="blog-email">hi@fragment.com</p>
                     <p className="blog-contact-info">Phone number</p>
                     <p className="blog-contact-info">Address</p>
                 </div>
 
-                <div className="blog-footer-column blog-footer-middle blog-offset-top">
+                <div className="blog-footer-column blog-footer-middle">
                     <a href="/">About Us</a>
                     <a href="/">Work with us</a>
                     <a href="/">FAQ's</a>
                 </div>
 
-                <div className="blog-footer-column blog-footer-right blog-offset-top">
+                <div className="blog-footer-column blog-footer-right">
                     <a href="/">Privacy</a>
                     <a href="/">Terms and Conditions</a>
                 </div>
@@ -52,3 +55,5 @@ const BlogFooter = () => {
 };
 
 export default BlogFooter;
+
+
