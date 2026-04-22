f724d0693f1df66bd8dc6f7b5cca60bc07b02223
Aishani Bandyopadhyay <aishanibandyopadhyay07@gmail.com>
2026-04-22 17:40:00 +0530

    Added two new pages after Page 1 for Organiser and changed the positions in navbar

--- src/Organiser.js
+++ src/Organiser.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Page1 from './Page1';
+import Page1b from './Page1b';
+import Page1c from './Page1c';
 import Page2 from './Page2';
 import Page3 from './Page3';
 import Page4 from './Page4';
@@ -15,6 +17,8 @@ const Organiser = ({ setView }) => {
   return (
     <div>
       <Page1 />
+      <Page1b />
+      <Page1c />
       <Page2 />
       <Page3 />
       <Page4 />
