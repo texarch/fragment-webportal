f0adf8478c6ea99df96e7a9eea9a7df8679cd483
Aishani Bandyopadhyay <aishanibandyopadhyay07@gmail.com>
2026-04-22 10:36:16 +0530

    Commented out Page16(Eni) from Individual and added a spacing between Page14 and Page17 of Individual

--- src/Individual.js
+++ src/Individual.js
@@ -3,7 +3,7 @@ import Page11 from './Page11';
 import Page12 from './Page12';
 import Page13 from './Page13';
 import Page14 from './Page14';
-import Page16 from './Page16';
+// import Page16 from './Page16';
 import Page17 from './Page17';
 import Page18 from './Page18';
 import Page19 from './Page19';
@@ -16,7 +16,8 @@ const Individual = ({ setView }) => {
       <Page12 />
       <Page13 />
       <Page14 />
-      <Page16 />
+      <div className="spacer-large"></div>
+      {/* <Page16 /> */}
       <Page17 />
       <Page18 />
       <Page19 />
